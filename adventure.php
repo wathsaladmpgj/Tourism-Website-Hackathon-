a1ac650a79434094f707303e1394fca9aaccbbb9
Janith Wathsala <janijaniwathsala@gmail.com>
2025-01-08 23:24:37 +0530

    activity page develop

---
+++ adventure.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adventure Awaits</title>
+    <link rel="stylesheet" href="./nav_fotter.css">
+    <link rel="stylesheet" href="./activity.css">
+</head>
+<body>
+    <nav class="navbar">
+        <ul class="nav-links">
+            <li><a href="./home_page.php">Home</a></li>
+            <li><a href="./destination_page_1.html">Destination</a></li>
+            <li><a href="#">Activities</a></li>
+            <li><a href="#">Foods</a></li>
+            <li><a href="#">Festival</a></li>
+            <li><a href="#">Hotel</a></li>
+            <li><a href="#">Plan Your Trip</a></li>
+            <li><a href="#">Blog</a></li>
+        </ul>
+    </nav>
+    <div class="hero">
+        <div class="image-grid">
+            <div class="image-item1">
+                <img src="./images/activity images/hero1.png" alt="Hot Air Balloon">
+                <img src="./images/activity images/hero2.png" alt="Hot Air Balloon">
+            </div>
+            <div class="image-item2">
+                <img src="./images/activity images/hero3.png" alt="Hot Air Balloon">
+                <img src="./images/activity images/hero4.png" alt="Hot Air Balloon">
+            </div>
+            <div class="text-overlay">
+                <h1>ADVENTURE AWAITS HERE</h1>
+            </div>
+            <<div class="image-item2">
+                <img src="./images/activity images/hero5.png" alt="Hot Air Balloon">
+                <img src="./images/activity images/hero6.png" alt="Hot Air Balloon">
+            </div>
+            <div class="image-item1">
+                <img src="./images/activity images/hero7.png" alt="Hot Air Balloon">
+                <img src="./images/activity images/hero8.png" alt="Hot Air Balloon">
+            </div>
+        </div>
+    </div>
+
+    <div class="details">
+        <h1>"Unleash Your Wild Side: Adventure Awaits in Sri Lanka"</h1>
+        <p>"Sri Lanka is your ultimate playground for adrenaline-pumping thrills and nature-filled escapades. <br> Whether you're conquering misty peaks, diving into vibrant underwater worlds, or gliding along <br> rolling waves, every corner of this island promises unforgettable experiences. Embark on epic <br> hiking trails, brave wild safaris in lush jungles, or feel the rush of water rafting through roaring rapids. <br> For those who seek a different kind of thrill, sea safaris, rope jumping, and bonfire nights under the <br> stars add magic to your adventure. Get ready to explore, challenge, and connect with Sri Lanka’s <br> untamed beauty."</p>
+    </div>
+
+    <div class="card-section">
+        <div class="card">
+            <h2>Hicking</h2>
+            <img src="./images/activity images/card1.png" alt="">
+            <p>Sri Lanka offers an enchanting hiking experience with lush rainforests, misty mountains, and breathtaking views. Explore trails like Adam’s Peak, Horton Plains, and Knuckles Range. Discover cascading waterfalls, exotic wildlife, and ancient ruins amidst nature's serene beauty. Adventure awaits!</p>
+            <button>Seamore</button>
+        </div>
+        <div class="card">
+            <h2>Diving</h2>
+            <img src="./images/activity images/card2.png" alt="">
+            <p>Sri Lanka is a diver’s paradise with crystal-clear waters, vibrant coral reefs, and historic shipwrecks. Explore spots like Hikkaduwa, Trincomalee, and Kalpitiya. Encounter colorful marine life, including turtles, rays, and reef sharks, for an unforgettable underwater adventure!</p>
+            <button>Seamore</button>
+        </div>
+        <div class="card">
+            <h2>Surfing</h2>
+            <img src="./images/activity images/card3.png" alt="">
+            <p>Sri Lanka is a surfing haven with pristine beaches and world-class waves. Arugam Bay, Weligama, and Hikkaduwa offer breaks for all skill levels. Enjoy warm waters, golden sands, and a laid-back surf culture in this tropical paradise!</p>
+            <button>Seamore</button>
+        </div>
+        <div class="card">
+            <h2>Safari</h2>
+            <img src="./images/activity images/card4.png" alt="">
+            <p>Sri Lanka’s safaris unveil a world of wild wonders. Explore Yala and Udawalawe National Parks to spot majestic elephants, elusive leopards, and vibrant birdlife. Witness untamed beauty, from lush jungles to golden grasslands, in thrilling wildlife adventures!</p>
+            <button>Seamore</button>
+        </div>
+    </div>
+    <div class="card-section">
+        <div class="card">
+            <h2>Water Rafting</h2>
+            <img src="./images/activity images/card5.png" alt="">
+            <p>Sri Lanka offers exhilarating white-water rafting adventures in lush, scenic settings. Paddle through the thrilling rapids of the Kelani River in Kitulgala, surrounded by verdant rainforests. Perfect for adventure seekers and nature lovers, it’s pure adrenaline-packed fun!</p>
+            <button>Seamore</button>
+        </div>
+        <div class="card">
+            <h2>Rope Jump</h2>
+            <img src="./images/activity images/card6.png" alt="">
+            <p>In Sri Lanka, rope jumping is an adrenaline-pumping adventure that challenges thrill-seekers to leap into breathtaking landscapes. Experience the rush in locations like Kitulgala, where lush rainforests and dramatic cliffs set the stage for this heart-pounding activity!</p>
+            <button>Seamore</button>
+        </div>
+        <div class="card">
+            <h2>Camping</h2>
+            <img src="./images/activity images/card7.png" alt="">
+            <p>Camping in Sri Lanka is a magical experience, blending nature and serenity. Pitch tents in stunning locations like Horton Plains, Knuckles Range, or Yala National Park. Sleep under starlit skies, wake to birdsong, and immerse in untamed wilderness!</p>
+            <button>Seamore</button>
+        </div>
+        <div class="card">
+            <h2>Bonfire</h2>
+            <img src="./images/activity images/card8.png" alt="">
+            <p>Sri Lanka offers an enchanting hiking experience with lush rainforests, misty mountains, and breathtaking views. Explore trails like Adam’s Peak, Horton Plains, and Knuckles Range. Discover cascading waterfalls, exotic wildlife, and ancient ruins amidst nature's serene beauty. Adventure awaits!</p>
+            <button>Seamore</button>
+        </div>
+    </div>
+    <div class="card-section">
+        <div class="card">
+            <h2>Ballon Rides</h2>
+            <img src="./images/activity images/card9.png" alt="">
+            <p>Sri Lanka offers an enchanting hiking experience with lush rainforests, misty mountains, and breathtaking views. Explore trails like Adam’s Peak, Horton Plains, and Knuckles Range. Discover cascading waterfalls, exotic wildlife, and ancient ruins amidst nature's serene beauty. Adventure awaits!</p>
+            <button>Seamore</button>
+        </div>
+        <div class="card">
+            <h2>Boat Rides</h2>
+            <img src="./images/activity images/card10.png" alt="">
+            <p>Sri Lanka offers an enchanting hiking experience with lush rainforests, misty mountains, and breathtaking views. Explore trails like Adam’s Peak, Horton Plains, and Knuckles Range. Discover cascading waterfalls, exotic wildlife, and ancient ruins amidst nature's serene beauty. Adventure awaits!</p>
+            <button>Seamore</button>
+        </div>
+    </div>
+
+
+    <footer>
+        <h1>hi</h1>
+    </footer>
+</body>
+</html>
